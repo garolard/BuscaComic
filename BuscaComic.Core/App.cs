c215e72c33921699643efec59e236e6ad9b03676
Gabriel Ferreiro <gbril9119@gmail.com>
2021-08-02 18:46:05 +0200

    Commit inicial

---
+++ BuscaComic.Core/App.cs
@@ -0,0 +1,16 @@
+﻿using MvvmCross.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BuscaComic.Core
+{
+    public class App : MvxApplication
+    {
+        public override void Initialize()
+        {
+            // Registrar dependencias
+            // Registrar ViewModel inicial
+        }
+    }
+}
