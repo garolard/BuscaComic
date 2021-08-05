6133b6b0e0ef089358d2fc93facf7b858f29455a
Gabriel Ferreiro <gbril9119@gmail.com>
2021-08-05 13:04:53 +0200

    Agregado proyecto de tests

---
+++ BuscaComic.Test/Mappers/CharacterToCharacterDTOMapperSpecs.cs
@@ -0,0 +1,46 @@
+﻿using BuscaComic.Core.DTOs;
+using BuscaComic.Core.Mappers;
+using BuscaComic.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace BuscaComic.Test.Mappers
+{
+    public class CharacterToCharacterDTOMapperSpecs
+    {
+        private readonly IMapper<Character, CharacterDTO> mapper;
+
+        public CharacterToCharacterDTOMapperSpecs()
+        {
+            mapper = new CharacterToCharacterDTOMapper();
+        }
+
+        [Fact]
+        public void Should_Map_A_Filled_Character()
+        {
+            var character = Character();
+            var dto = mapper.Map(character);
+
+            Assert.Equal(character.Name, dto.Name);
+            Assert.Equal(character.Description, dto.Description);
+            Assert.Equal($"{character.Thumbnail.Path}.{character.Thumbnail.Extension}", dto.ImageUrl);
+        }
+
+        private Character Character()
+        {
+            return new Character
+            {
+                Id = 1,
+                Name = "Captain America",
+                Description = "An American super-soldier",
+                Thumbnail = new Thumbnail
+                {
+                    Path = new Uri("https://placeholder.pics/svg/300"),
+                    Extension = "jpg"
+                }
+            };
+        }
+    }
+}
